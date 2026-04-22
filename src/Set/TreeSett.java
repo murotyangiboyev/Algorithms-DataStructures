123d3632e6d5dae85fe9a8d249a418a038b9c21e
Murot Yangiboyev <murotyangiboyev77@gmail.com>
2026-04-22 23:25:15 +0500

    TreeSet (2)

--- src/Set/TreeSett.java
+++ src/Set/TreeSett.java
@@ -1,5 +1,11 @@
 package Set;
 
+import com.sun.source.tree.Tree;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class TreeSett {
@@ -13,7 +19,27 @@ public class TreeSett {
         treeSet.add(0);
         treeSet.add(4);
 
+        int first = treeSet.first();
+        int last = treeSet.last();
+        int lower = treeSet.lower(4);
+        int higher = treeSet.higher(4);
+
+        System.out.println(lower + " "  + higher);
+
+        treeSet.pollFirst();
+        treeSet.pollLast();
         System.out.println(treeSet);
+
+        Iterator itr = treeSet.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+
+        treeSet.addAll(Arrays.asList(10, 70, 50, 6, 7 , 10));
+        System.out.println(treeSet);
+
+        SortedSet<Integer> set2  = treeSet.subSet(10, 51);
+        System.out.println(set2);
     }
 
 

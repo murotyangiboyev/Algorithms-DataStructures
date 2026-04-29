f6e2064a2b22208b60c15fc8683136391d465e07
Murot Yangiboyev <murotyangiboyev77@gmail.com>
2026-04-29 22:29:48 +0500

    PriorityQueue

--- src/Queue/PriorityQueuee.java
+++ src/Queue/PriorityQueuee.java
@@ -1,10 +1,11 @@
 package Queue;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PriorityQueuee {
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
 
         pq.add(3);
         pq.add(0);
